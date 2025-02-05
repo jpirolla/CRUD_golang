4b2af13623373d0a79898d2bb39918c7233bc717
Juliana Pirolla <jpirollap@gmail.com>
2025-02-05 14:56:07 -0300

    add validator

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,22 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(C *gin.Context) {
-	//c.Request.
+	"github.com/gin-gonic/gin"
+	"github.com/jpirolla/CRUD_golang/src/configuration/rest_err"
+	"github.com/jpirolla/CRUD_golang/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("Incorrect filds, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println((userRequest))
 }
