7f398e111f77a65f032e131cf9c38d1d44ee7494
Juliana Pirolla <jpirollap@gmail.com>
2025-02-26 10:09:39 -0300

    fluxo de updateUser

--- src/model/repository/entity/user_entity.go
+++ src/model/repository/entity/user_entity.go
@@ -1,9 +1,11 @@
 package entity
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type UserEntity struct {
-	ID       string `bson:" id.omitempty"`
-	Email    string `bson:"email"`
-	Password string `bson:"password"`
-	Name     string `bson:"name"`
-	Age      int8   `bson:"age"`
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Email    string             `bson:"email,omitempty"`
+	Password string             `bson:"password,omitempty"`
+	Name     string             `bson:"name,omitempty"`
+	Age      int8               `bson:"age,omitempty"`
 }
