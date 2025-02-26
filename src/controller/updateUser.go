210a9967c30136e34b5cf54be298a62cfe607cea
Juliana Pirolla <jpirollap@gmail.com>
2025-02-26 10:03:47 -0300

    add swagger api

--- src/controller/updateUser.go
+++ src/controller/updateUser.go
@@ -1,7 +1,70 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/jpirolla/CRUD_golang/src/configuration/logger"
+	"github.com/jpirolla/CRUD_golang/src/configuration/rest_err"
+	"github.com/jpirolla/CRUD_golang/src/configuration/validation"
+	"github.com/jpirolla/CRUD_golang/src/controller/model/request"
+	"github.com/jpirolla/CRUD_golang/src/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+)
+
+// UpdateUser updates user information with the specified ID.
+// @Summary Update User
+// @Description Updates user details based on the ID provided as a parameter.
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param userId path string true "ID of the user to be updated"
+// @Param userRequest body request.UserUpdateRequest true "User information for update"
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Success 200
+// @Failure 400 {object} rest_err.RestErr
+// @Failure 500 {object} rest_err.RestErr
+// @Router /updateUser/{userId} [put]
 func (uc *userControllerInterface) UpdateUser(c *gin.Context) {
-	//c.Request.
+	logger.Info("Init updateUser controller",
+		zap.String("journey", "updateUser"),
+	)
+	var userRequest request.UserUpdateRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "updateUser"))
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	userId := c.Param("userId")
+	if _, err := primitive.ObjectIDFromHex(userId); err != nil {
+		errRest := rest_err.NewBadRequestError("Invalid userId, must be a hex value")
+		c.JSON(errRest.Code, errRest)
+	}
+
+	domain := model.NewUserUpdateDomain(
+		userRequest.Name,
+		userRequest.Age,
+	)
+	err := uc.service.UpdateUser(userId, domain)
+	if err != nil {
+		logger.Error(
+			"Error trying to call updateUser service",
+			err,
+			zap.String("journey", "updateUser"))
+		c.JSON(err.Code, err)
+		return
+	}
+
+	logger.Info(
+		"updateUser controller executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "updateUser"))
+
+	c.Status(http.StatusOK)
 }
--- src/controller/user_controller.go
+++ src/controller/user_controller.go
@@ -16,9 +16,11 @@ func NewUserControllerInterface(
 type UserControllerInterface interface {
 	FindUserByID(c *gin.Context)
 	FindUserByEmail(c *gin.Context)
+
+	DeleteUser(c *gin.Context)
 	CreateUser(c *gin.Context)
 	UpdateUser(c *gin.Context)
-	DeleteUser(c *gin.Context)
+	LoginUser(c *gin.Context)
 }
 
 type userControllerInterface struct {
