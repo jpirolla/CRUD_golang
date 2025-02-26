ad9cd1504d8136e9a2e4d9dc85a53f8508788678
Juliana Pirolla <jpirollap@gmail.com>
2025-02-26 09:58:23 -0300

    verificação do token

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -3,16 +3,22 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jpirolla/CRUD_golang/src/controller"
+	"github.com/jpirolla/CRUD_golang/src/model"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRoutes(
 	r *gin.RouterGroup,
 	userController controller.UserControllerInterface) {
 
-	// como quero receber um parametro, coloco : na variavel userId na rota
-	r.GET("/getUserById/:userId", userController.FindUserByID)
-	r.GET("/getUserByEmail/:userEmail", userController.FindUserByEmail)
+	r.GET("/getUserById/:userId", model.VerifyTokenMiddleware, userController.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", model.VerifyTokenMiddleware, userController.FindUserByEmail)
 	r.POST("/createUser", userController.CreateUser)
-	r.PUT("/updateUser/:userId", userController.UpdateUser)
-	r.DELETE("/deletUser/:userId", userController.DeleteUser)
+	r.PUT("/updateUser/:userId", model.VerifyTokenMiddleware, userController.UpdateUser)
+	r.DELETE("/deleteUser/:userId", model.VerifyTokenMiddleware, userController.DeleteUser)
+
+	r.POST("/login", userController.LoginUser)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
