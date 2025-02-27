7c367f8fdb2022cc9b7ac13e6b3c7397a5ee0f11
Juliana Pirolla <jpirollap@gmail.com>
2025-02-27 09:48:34 -0300

    rota de login

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -1,24 +1,36 @@
 package service
 
 import (
-	"fmt"
-
 	"github.com/jpirolla/CRUD_golang/src/configuration/logger"
 	"github.com/jpirolla/CRUD_golang/src/configuration/rest_err"
 	"github.com/jpirolla/CRUD_golang/src/model"
 	"go.uber.org/zap"
 )
 
-// para dependencias externas
-
-func (ud *userDomainService) CreateUser(
+func (ud *userDomainService) CreateUserServices(
 	userDomain model.UserDomainInterface,
-) *rest_err.RestErr {
+) (model.UserDomainInterface, *rest_err.RestErr) {
 
-	logger.Info("Init createUser controller", zap.String("journey", "createUser"))
+	logger.Info("Init createUser model.",
+		zap.String("journey", "createUser"))
+
+	user, _ := ud.FindUserByEmailServices(userDomain.GetEmail())
+	if user != nil {
+		return nil, rest_err.NewBadRequestError("Email is already registered in another account")
+	}
 
 	userDomain.EncryptPassword()
-	fmt.Println(userDomain.GetPassword())
+	userDomainRepository, err := ud.userRepository.CreateUser(userDomain)
+	if err != nil {
+		logger.Error("Error trying to call repository",
+			err,
+			zap.String("journey", "createUser"))
+		return nil, err
+	}
 
-	return nil
+	logger.Info(
+		"CreateUser service executed successfully",
+		zap.String("userId", userDomainRepository.GetID()),
+		zap.String("journey", "createUser"))
+	return userDomainRepository, nil
 }
