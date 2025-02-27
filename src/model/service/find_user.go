fd97117ee7f8cbf5be12773cfc05dc5c6133daea
Juliana Pirolla <jpirollap@gmail.com>
2025-02-27 09:52:38 -0300

    incluindo busca por id, email

--- src/model/service/find_user.go
+++ src/model/service/find_user.go
@@ -1,11 +1,36 @@
 package service
 
 import (
+	"github.com/jpirolla/CRUD_golang/src/configuration/logger"
 	"github.com/jpirolla/CRUD_golang/src/configuration/rest_err"
 	"github.com/jpirolla/CRUD_golang/src/model"
+	"go.uber.org/zap"
 )
 
-func (*userDomainService) FindUser(string) (
-	*model.UserDomainInterface, *rest_err.RestErr) {
-	return nil, nil
+func (ud *userDomainService) FindUserByIDServices(
+	id string,
+) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByID services.",
+		zap.String("journey", "findUserById"))
+
+	return ud.userRepository.FindUserByID(id)
+}
+
+func (ud *userDomainService) FindUserByEmailServices(
+	email string,
+) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByEmail services.",
+		zap.String("journey", "findUserById"))
+
+	return ud.userRepository.FindUserByEmail(email)
+}
+
+func (ud *userDomainService) findUserByEmailAndPasswordServices(
+	email string,
+	password string,
+) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByEmail services.",
+		zap.String("journey", "findUserById"))
+
+	return ud.userRepository.FindUserByEmailAndPassword(email, password)
 }
