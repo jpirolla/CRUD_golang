4bedd6d15799403f93bea04794fc0c391210debe
Juliana Pirolla <jpirollap@gmail.com>
2025-02-27 10:08:17 -0300

    corrigindo importação do jwl

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,10 +1,5 @@
 package model
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 type userDomain struct {
 	id       string
 	email    string
@@ -13,29 +8,26 @@ type userDomain struct {
 	age      int8
 }
 
-func (ud *userDomain) SetID(id string) {
-	ud.id = id
+func (ud *userDomain) GetID() string {
+	return ud.id
 }
 
-func (ud *userDomain) GetJSONValue() (string, error) {
-	b, err := json.Marshal(ud)
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-	return string(b), nil
-
+func (ud *userDomain) SetID(id string) {
+	ud.id = id
 }
 
 func (ud *userDomain) GetEmail() string {
 	return ud.email
 }
+
 func (ud *userDomain) GetPassword() string {
 	return ud.password
 }
+
 func (ud *userDomain) GetName() string {
 	return ud.name
 }
+
 func (ud *userDomain) GetAge() int8 {
 	return ud.age
 }
